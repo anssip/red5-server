c54b6a58ceb7d819cd124d8015513428ec79e8a3
Anssi Piirainen <anssi.piirainen@augment-sports.com>
2019-07-30 14:07:35 +0300

    Progress with wrapping the MultiThreadedApplicationAdapter

--- src/main/java/org/red5/server/plugin/javascript/ApplicationJs.java
+++ src/main/java/org/red5/server/plugin/javascript/ApplicationJs.java
@@ -1,10 +1,13 @@
 package org.red5.server.plugin.javascript;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.graalvm.polyglot.Value;
+import org.red5.server.adapter.IApplication;
 import org.red5.server.adapter.MultiThreadedApplicationAdapter;
 import org.red5.server.api.IClient;
 import org.red5.server.api.IConnection;
@@ -29,11 +32,16 @@ import org.red5.server.api.stream.IStreamAwareScopeHandler;
 import org.red5.server.api.stream.IStreamPlaybackSecurity;
 import org.red5.server.api.stream.IStreamPublishSecurity;
 import org.red5.server.api.stream.ISubscriberStream;
+import org.red5.server.jmx.mxbeans.ApplicationMXBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ApplicationJs {
-
+    protected static Logger log = LoggerFactory.getLogger(ApplicationJs.class);
     private final MultiThreadedApplicationAdapter app;
     private final JavaScriptPlugin plugin;
+    private final List<IStreamPublishSecurity> publishSecurities = new ArrayList<IStreamPublishSecurity>();
+    private final List<IStreamPlaybackSecurity> playbackSecurities = new ArrayList<IStreamPlaybackSecurity>();
 
     public ApplicationJs(MultiThreadedApplicationAdapter app, JavaScriptPlugin plugin) {
         this.app = app;
@@ -48,34 +56,72 @@ public class ApplicationJs {
         throw new UnsupportedOperationException();
     }
 
-    public void registerStreamPublishSecurity(Value isPublishAllowedCallback) {
-        this.app.registerStreamPublishSecurity(new IStreamPublishSecurity() {
+    public int registerStreamPublishSecurity(Value isPublishAllowedCallback) {
+        IStreamPublishSecurity security = new IStreamPublishSecurity() {
             @Override
             public boolean isPublishAllowed(IScope scope, String name, String mode) {
                 return plugin.executeInContext(isPublishAllowedCallback).asBoolean();
             }
-        });
+        };
+        this.app.registerStreamPublishSecurity(security);
+        publishSecurities.add(security);
+        return publishSecurities.indexOf(security);
     }
 
-    public void unregisterStreamPublishSecurity(IStreamPublishSecurity handler) {
-
+    public void unregisterStreamPublishSecurity(int index) {
+        log.debug("unregisterStreamPublishSecurity");
+        IStreamPublishSecurity security = publishSecurities.get(index);
+        if (security == null) {
+            return;
+        }
+        this.app.unregisterStreamPublishSecurity(security);
+        publishSecurities.set(index, null); // don't remove because that would shift the indexes
     }
 
-    public void registerStreamPlaybackSecurity(Value isPlaybackAllowedCallback) {
-        this.app.registerStreamPlaybackSecurity(new IStreamPlaybackSecurity() {
+    public int registerStreamPlaybackSecurity(Value isPlaybackAllowedCallback) {
+        IStreamPlaybackSecurity security = new IStreamPlaybackSecurity() {
             @Override
             public boolean isPlaybackAllowed(IScope scope, String name, int start, int length, boolean flushPlaylist) {
                 return plugin.executeInContext(isPlaybackAllowedCallback).asBoolean();
             }
-        });
+        };
+        this.app.registerStreamPlaybackSecurity(security);
+        playbackSecurities.add(security);
+        return playbackSecurities.indexOf(security);
     }
 
-    public void unregisterStreamPlaybackSecurity(IStreamPlaybackSecurity handler) {
-
+    public void unregisterStreamPlaybackSecurity(int index) {
+        log.debug("unregisterStreamPlaybackSecurity");
+        IStreamPlaybackSecurity security = playbackSecurities.get(index);
+        if (security == null) {
+            return;
+        }
+        this.app.unregisterStreamPlaybackSecurity(security);
+        playbackSecurities.set(index, null); // don't remove because that would shift the indexes
     }
 
     public Set<IStreamPublishSecurity> getStreamPublishSecurity() {
-        return null;
+        return new HashSet<IStreamPublishSecurity>(publishSecurities);
+    }
+
+    /**
+     * Register a listener that will get notified about application events.
+     * 
+     * TODO: to intercept connections you can add an IApplication listener.
+     * https://github.com/rajdeeprath/red5-development-series/blob/master/code-examples/server-side/red5-plugin-examples/hello-red5-plugin/src/main/java/org/red5/server/plugin/examples/hellored5/HelloRed5Plugin.java
+     * 
+     * @param listener object to register
+     */
+    public void addListener(IApplication listener) {
+    }
+
+    /**
+     * Unregister handler that will not get notified about application events any
+     * longer.
+     * 
+     * @param listener object to unregister
+     */
+    public void removeListener(IApplication listener) {
     }
 
     public boolean connect(IConnection conn, IScope scope, Object[] params) {
@@ -252,6 +298,54 @@ public class ApplicationJs {
 
     public boolean clearSharedObjects(IScope scope, String name) {
         return false;
+    }
+
+    public boolean appStart(IScope app) {
+        return false;
+    }
+
+    public boolean appConnect(IConnection conn, Object[] params) {
+        return false;
+    }
+
+    public boolean appJoin(IClient client, IScope app) {
+        return false;
+    }
+
+    public void appDisconnect(IConnection conn) {
+
+    }
+
+    public void appLeave(IClient client, IScope app) {
+
+    }
+
+    public void appStop(IScope app) {
+
+    }
+
+    public boolean roomStart(IScope room) {
+        return false;
+    }
+
+    public boolean roomConnect(IConnection conn, Object[] params) {
+        return false;
+    }
+
+    public boolean roomJoin(IClient client, IScope room) {
+        return false;
+    }
+
+    public void roomDisconnect(IConnection conn) {
+
+    }
+
+    public void roomLeave(IClient client, IScope room) {
+
+    }
+
+    public void roomStop(IScope room) {
+
     }
 
 }
